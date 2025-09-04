9597ece4d977045a9944c48b01212825e49883dc
Tegardc <tegardimas2022@gmail.com>
2025-09-04 15:04:48 +0800

    feat: Update questions in QuestionsSeeder for improved clarity and relevance

--- database/seeders/QuestionsSeeder.php
+++ database/seeders/QuestionsSeeder.php
@@ -14,11 +14,11 @@ class QuestionsSeeder extends Seeder
     public function run(): void
     {
         $questions = [
-            'Seberapa baik fasilitas yang disediakan oleh sekolah?',
-            'Seberapa kompeten guru-guru dalam menyampaikan materi?',
-            'Bagaimana pendapat Anda tentang kebersihan dan kenyamanan lingkungan sekolah?',
-            'Apakah komunikasi antara pihak sekolah dan orang tua berjalan dengan baik?',
-            'Seberapa puas Anda terhadap kegiatan ekstrakurikuler yang disediakan sekolah?',
+            'Kelengkapan Fasilitas',
+            'Proses Pembelajaran',
+            'Pelayanan Sekolah',
+            'Pelayanan Keamanan',
+            'Ekstrakulikuler',
         ];
 
         foreach ($questions as $question) {
