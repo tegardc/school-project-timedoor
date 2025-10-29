9eaed6937f94d967aede046193a73be7e8d9a9bb
Tegardc <tegardimas2022@gmail.com>
2025-10-29 20:17:16 +0800

    feat: Add new restore

--- routes/api.php
+++ routes/api.php
@@ -63,6 +63,8 @@ use App\Models\SubDistrict;
             Route::get('/school-details/saved', [SchoolDetailController::class, 'showSaved']);
             Route::put('/profile/complete', [UserController::class, 'profileStore']);
 
+                        Route::post('/users/{id}', [UserController::class, 'restore']);
+                                    Route::get('/users', [UserController::class, 'trash']);
         });
 
         //ROUTE FOR ADMIN ROLE//
@@ -98,7 +100,7 @@ use App\Models\SubDistrict;
 
             //Trash and Restore Route
             Route::prefix('trash')->group(function () {
-            Route::get('/users', [UserController::class, 'trash']);
+
             Route::get('/schools', [SchoolController::class, 'trash']);
             Route::get('/school-details', [SchoolDetailController::class, 'trash']);
             Route::get('/districts', [DistrictController::class, 'trash']);
@@ -112,7 +114,7 @@ use App\Models\SubDistrict;
         });
 
         Route::prefix('restore')->group(function () {
-            Route::post('/users/{id}', [UserController::class, 'restore']);
+
             Route::post('/schools/{id}', [SchoolController::class, 'restore']);
             Route::post('/school-details/{id}', [SchoolDetailController::class, 'restore']);
             Route::post('/districts/{id}', [DistrictController::class, 'restore']);
