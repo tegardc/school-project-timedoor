3571480ce9d78aec9c631b8566799b938b4d448a
Tegardc <tegardimas2022@gmail.com>
2026-01-12 21:00:45 +0800

    feat: add schoolValidation field to educationExperiences in UserResource

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -61,6 +61,7 @@ class UserResource extends JsonResource
                                 'id'           => $edu->id,
                                 'schoolDetail' => optional($edu->schoolDetail)->name,
                                 'status'       => $edu->status,
+                                'schoolValidation' => $this->schoolValidation,
                             ];
                         }),
                     ];
